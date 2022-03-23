cf14d54cd464dcb916e9616fbb3e568712f1f663
Omri Tamam <omri.tamam@cs.huji.ac.il>
2022-03-23 13:17:26 +0200

    /iterations

--- ex1/main.c
+++ ex1/main.c
@@ -4,8 +4,8 @@
 
 int main()
 {
-    printf("osm_function_time is %lf\n", osm_function_time(pow(10,6)));
-    printf("osm_operation_time is %lf\n", osm_operation_time(pow(10,6)));
-    printf("osm_syscall_time is %lf\n", osm_syscall_time(pow(10,6)));
+    printf("osm_operation_time is %lf\n", osm_operation_time(pow(10,5)));
+    printf("osm_function_time is %lf\n", osm_function_time(pow(10,5)));
+    printf("osm_syscall_time is %lf\n", osm_syscall_time(pow(10,5)));
     return 0;
 }
--- ex1/osm.c
+++ ex1/osm.c
@@ -1,49 +1,46 @@
-
 #include <time.h>
 #include "osm.h"
 #include <sys/time.h>
 
+int do_nothing(){
+    return 0;
+}
 
-void do_nothing(){}
-
-double osm_operation_time(unsigned int iterations)
-{
+double osm_operation_time(unsigned int iterations){
+    if(iterations <= 0 ){
+        return -1;
+    }
     struct timeval begin, end;
     unsigned int iteration = 0;
     gettimeofday(&begin, 0);
+    int temp1, temp2, temp3, temp4, temp5, temp6, temp7, temp8, temp9, temp10;
     while(iteration < iterations)
 	{
-        iteration++;
-        iteration++;
-        iteration++;
-        iteration++;
-        iteration++;
-        iteration++;
-        iteration++;
-        iteration++;
-        iteration++;
-        iteration++;
+        iteration +=10;
+        temp1++;
+        temp2++;
+        temp3++;
+        temp4++;
+        temp5++;
+        temp6++;
+        temp7++;
+        temp8++;
+        temp9++;
+        temp10++;
     }
     gettimeofday(&end, 0);
-    long microseconds = (end.tv_usec - begin.tv_usec);
-    return (double) microseconds*1000;
+    return ((double)(end.tv_usec - begin.tv_usec)) * ((double) 1000 / iterations);
 }
 double osm_syscall_time(unsigned int iterations){
+    if(iterations <= 0 ){
+        return -1;
+    }
     struct timeval begin, end;
     unsigned int iteration = 0;
     gettimeofday(&begin, 0);
     while(iteration < iterations)
     {
-        iteration++;
-        iteration++;
-        iteration++;
-        iteration++;
-        iteration++;
-        iteration++;
-        iteration++;
-        iteration++;
-        iteration++;
-        iteration++;
+        iteration+=10;
         OSM_NULLSYSCALL;
         OSM_NULLSYSCALL;
         OSM_NULLSYSCALL;
@@ -56,25 +53,18 @@ double osm_syscall_time(unsigned int iterations){
         OSM_NULLSYSCALL;
     }
     gettimeofday(&end, 0);
-    long microseconds = (end.tv_usec - begin.tv_usec);
-    return (double) microseconds*1000;
+    return ((double)(end.tv_usec - begin.tv_usec)) * ((double) 1000 / iterations);
 }
 double osm_function_time(unsigned int iterations){
+    if(iterations <= 0 ){
+        return -1;
+    }
     struct timeval begin, end;
     unsigned int iteration = 0;
     gettimeofday(&begin, 0);
     while(iteration < iterations)
     {
-        iteration++;
-        iteration++;
-        iteration++;
-        iteration++;
-        iteration++;
-        iteration++;
-        iteration++;
-        iteration++;
-        iteration++;
-        iteration++;
+        iteration+=10;
         do_nothing();
         do_nothing();
         do_nothing();
@@ -87,6 +77,5 @@ double osm_function_time(unsigned int iterations){
         do_nothing();
     }
     gettimeofday(&end, 0);
-    long microseconds = (end.tv_usec - begin.tv_usec);
-    return (double) microseconds*1000;
+    return ((double)(end.tv_usec - begin.tv_usec)) * ((double) 1000 / iterations);
 }
